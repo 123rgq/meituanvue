7d866400001c3d9214273a197a51d87dbd58d21a
raogq <1257823551@qq.com>
2019-04-02 17:28:18 +0800

    测试页面提交

---
+++ src/components/Pages.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>子也米娜</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'测试页面'
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Pages from '@/components/HelloWorld'
 
 Vue.use(Router)
 
@@ -10,6 +10,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/pages',
+      name: 'pages',
+      component: Pages
     }
   ]
 })
